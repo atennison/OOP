f929ff45b91077a4b6a80b4bb2cbbefc01cb6309
atennison <atennison77@gmail.com>
2026-08-28 09:03:40 -0500

    Add files via upload

---
+++ PP -1.py
@@ -0,0 +1,12 @@
+print("Enter your name:")
+a = input()
+
+print("Enter your basic pay(whole number):")
+b = int(input())
+
+print("Enter your deductions(whole number):")
+c = int(input())
+
+d = b - c
+
+print(a ,"makes a total of", d, "dollars")
